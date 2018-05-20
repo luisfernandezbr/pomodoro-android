4067d7b6608fd14bfe3b34da834c1c530736b844
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-20 09:58:42 -0300

    First commit

---
+++ app/src/main/java/br/com/luisfernandez/pomodoro/HomeActivity.kt
@@ -0,0 +1,19 @@
+package br.com.luisfernandez.pomodoro
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.luisfernandez.pomodoro.ui.home.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, HomeFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
