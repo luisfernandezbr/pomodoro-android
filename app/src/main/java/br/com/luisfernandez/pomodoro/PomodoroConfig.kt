983dd6307171d9e6f8394922223b6de298a7f36e
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-20 16:24:42 -0300

    Added PomodoroConfigs class

---
+++ app/src/main/java/br/com/luisfernandez/pomodoro/PomodoroConfig.kt
@@ -0,0 +1,8 @@
+package br.com.luisfernandez.pomodoro
+
+class PomodoroConfig {
+    companion object {
+        const val POMODORO_TIME_IN_MILLIS = 60 * 1000L
+        const val TIMER_COUNT_INTERVAL = 1000L
+    }
+}
